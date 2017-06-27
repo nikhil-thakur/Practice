fec78e52ab5847302e446b2e25bf58990a1de367
Nik Thakur <nik.thakur@daugherty.com>
2017-06-27 16:40:33 -0500

    Prime Numbers: Written the algorithm. Need to write another method that works for SPOJ

---
+++ src/PrimeNumbers/PrimeNumbers.java
@@ -0,0 +1,29 @@
+package PrimeNumbers;
+
+import java.util.Arrays;
+
+/**
+ * Created by nikhilthakur on 6/27/17.
+ * This is using Sieve of Eratosthenes and is completed in a complexity of O(n.log log n)
+ * If the number hasn't been crossed of till the square root of itself, then its a prime number
+ */
+public class PrimeNumbers {
+
+    public static Boolean[] findPrimes(int endIndexNumber) {
+        Boolean[] prime = new Boolean[endIndexNumber+1];
+
+        Arrays.fill(prime,true);
+        prime[0] = false;
+        prime[1] = false;
+
+        for (int positiveInteger = 2; positiveInteger <= Math.sqrt(endIndexNumber); positiveInteger++){
+            if (prime[positiveInteger]){
+                for (int multiple = 2; positiveInteger*multiple <= endIndexNumber; multiple++){
+                    prime[positiveInteger*multiple] = false;
+                }
+            }
+        }
+
+        return prime;
+    }
+}
---
+++ src/PrimeNumbers/PrimeTest.java
@@ -0,0 +1,29 @@
+package PrimeNumbers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by nikhilthakur on 6/27/17.
+ */
+public class PrimeTest {
+
+
+    public static final int NUMBER_TO_CALCULATE_PRIMES_UNTIL = 10;
+
+    @Test
+    public void primeArrayLengthShouldBeOneGreaterThanTheInput() {
+        Assert.assertEquals(NUMBER_TO_CALCULATE_PRIMES_UNTIL +1, PrimeNumbers.findPrimes(NUMBER_TO_CALCULATE_PRIMES_UNTIL).length);;
+    }
+
+    @Test
+    public void primeArrayofInputNumberTenShouldHaveAtleastOneIndexWithAPrime() {
+        Boolean aBoolean = PrimeNumbers.findPrimes(NUMBER_TO_CALCULATE_PRIMES_UNTIL)[3];
+        Assert.assertTrue(aBoolean.equals(true));
+    }
+
+
+
+}
