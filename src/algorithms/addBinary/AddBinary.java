44c59475a332f0283516b66b2890fb09642ecc6b
Nik Thakur <nik.thakur@daugherty.com>
2018-08-02 14:31:59 -0500

    Will get this done later

---
+++ src/algorithms/addBinary/AddBinary.java
@@ -0,0 +1,12 @@
+package algorithms.addBinary;
+
+/**
+ * TODO: Easy stuff. Will work on this later when there is nothing to do
+ */
+public class AddBinary {
+    public String addBinary(String a, String b) {
+        char[] one = a.toCharArray();
+        char[] two = b.toCharArray();
+        return null;
+    }
+}
---
+++ src/algorithms/addBinary/AddBinaryTest.java
@@ -0,0 +1,12 @@
+package algorithms.addBinary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddBinaryTest {
+    @Test
+    public void addBinary() throws Exception {
+    }
+
+}
