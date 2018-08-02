dd544e810af2600dde1793892306a0eba6fdf656
Nik Thakur <nik.thakur@daugherty.com>
2018-08-02 15:04:01 -0500

    Submission Intersection Array

--- src/algorithms/intersectionArray/IntersectionArray.java
+++ src/algorithms/intersectionArray/IntersectionArray.java
@@ -1,4 +1,32 @@
 package algorithms.intersectionArray;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class IntersectionArray {
+        public int[] intersection(int[] nums1, int[] nums2) {
+            List<Integer> resultList = new ArrayList<>();
+            Set<Integer> firstSet = new HashSet<>();
+            for(Integer number: nums1){
+                firstSet.add(number);
+            }
+
+            Set<Integer> secondSet = new HashSet<>();
+            for(Integer number: nums2){
+                secondSet.add(number);
+            }
+
+            for(Integer number: secondSet){
+                if(firstSet.contains(number)) resultList.add(number);
+            }
+
+            int[] returnArray = new int[resultList.size()];
+            for(int index = 0; index< resultList.size() ; index++) {
+                returnArray[index] = resultList.get(index);
+            }
+
+            return returnArray;
+        }
 }
--- src/algorithms/intersectionArray/IntersectionArrayTest.java
+++ src/algorithms/intersectionArray/IntersectionArrayTest.java
@@ -1,7 +1,28 @@
 package algorithms.intersectionArray;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class IntersectionArrayTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @Test
+    public void intersection() throws Exception {
+        int[] array1 = new int[] {1};
+        int[] array2 = new int[] {1};
+
+        IntersectionArray intersectionArray = new IntersectionArray();
+        int[] result = intersectionArray.intersection(array1, array2);
+        Assert.assertEquals(1, result[0]);
+
+    }
 
 }
