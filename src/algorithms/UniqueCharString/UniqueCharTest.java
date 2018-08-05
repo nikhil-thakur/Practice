89307a7fe87b3bc1a5d0f6e11a248840a5f07a9a
Nik Thakur <nik.thakur@daugherty.com>
2018-08-04 19:24:05 -0500

    Added test for Unique Char

--- src/algorithms/UniqueCharString/UniqueCharTest.java
+++ src/algorithms/UniqueCharString/UniqueCharTest.java
@@ -1,7 +1,31 @@
 package algorithms.UniqueCharString;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UniqueCharTest {
+    @Test
+    public void testForNoUniqueValue() throws Exception {
+        int aaaa = UniqueChar.computeFirstUniqueElement("aaaa");
+        Assert.assertEquals("Should be returning -1 when no unique element is found in the string",-1 , aaaa);
+    }
+
+    @Test
+    public void testForNoOneValue() throws Exception {
+        int aaaa = UniqueChar.computeFirstUniqueElement("cabc");
+        Assert.assertEquals("This input has character a as the first unique element, therefore 1 should be returned",1,aaaa);
+    }
+
+    @Test
+    public void testForEmpty() throws Exception {
+        int aaaa = UniqueChar.computeFirstUniqueElement("");
+        Assert.assertEquals("If a empty string is passed, -1 should be returned",-1 ,aaaa);
+    }
+
+    @Test
+    public void testForNull() throws Exception {
+        int aaaa = UniqueChar.computeFirstUniqueElement(null);
+        Assert.assertEquals("If a null string is passed, -1 should be returned", -1,aaaa );
+    }
 
 }
