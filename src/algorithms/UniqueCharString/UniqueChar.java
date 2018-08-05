b09b7f41de5fb0f3ed27f973c97eddb294a07009
Nik Thakur <nik.thakur@daugherty.com>
2018-08-04 20:00:53 -0500

    Added code to satisfy the tests.

--- src/algorithms/UniqueCharString/UniqueChar.java
+++ src/algorithms/UniqueCharString/UniqueChar.java
@@ -1,5 +1,9 @@
 package algorithms.UniqueCharString;
 
+import com.sun.deploy.util.StringUtils;
+
+import java.util.*;
+
 /**
  * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
  *
@@ -12,4 +16,24 @@ package algorithms.UniqueCharString;
  Note: You may assume the string contain only lowercase letters.
  */
 public class UniqueChar {
+    public static int computeFirstUniqueElement(String input) {
+        if (input == null || input.isEmpty()) return -1;
+        char[] chars = input.toCharArray();
+        Map<Character, Integer> result = new LinkedHashMap<>();
+        Set<Character> uniqueness = new HashSet<>();
+
+
+        for (int index = 0; index< chars.length; index++){
+            if (uniqueness.contains(chars[index])){
+                result.remove(chars[index]);
+                continue;
+            }
+            else {
+                result.put(chars[index], index);
+                uniqueness.add(chars[index]);
+            }
+        }
+
+        return result.size() == 0 ? -1 : result.entrySet().iterator().next().getValue();
+    }
 }
